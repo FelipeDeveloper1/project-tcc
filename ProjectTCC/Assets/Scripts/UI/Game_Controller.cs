f59e6184debcbb2712cea44b1fcd7f6295e31067
garotodeprograma <110305250+LunarPhos@users.noreply.github.com>
2022-09-10 18:00:36 -0300

    Add files via upload

--- ProjectTCC/Assets/Scripts/UI/Game_Controller.cs
+++ ProjectTCC/Assets/Scripts/UI/Game_Controller.cs
@@ -1,28 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Game_Controller : MonoBehaviour {
 
     public static Game_Controller instance;
+    public Vector2 lastCheckPointPos;
 
-    void Start() {
-        instance = this;
-    }
-
-    void Update() {
-
-        // Chama o "Reload"
-        if(Input.GetKey(KeyCode.R) && Player.pr.blockInput) {
-            Reload();
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(instance);
         }
 
+        else {
+            Destroy(gameObject);
+        }
     }
-
-    // Reinicia a cena
-    public void Reload() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-    
 }
